32376e4d8b364278384c591694bb99f70d03cc79
FalseR20 <kno3.2002@gmail.com>
2024-01-26 18:57:15 +0300

    feat: add admin

--- digital_diary/diary/admin.py
+++ digital_diary/diary/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year')
+
+
+@admin.register(models.Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(models.Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ('course', 'subject', 'teacher')
+
+
+@admin.register(models.Mark)
+class MarkAdmin(admin.ModelAdmin):
+    list_display = ('student', 'value', 'program', 'date')
